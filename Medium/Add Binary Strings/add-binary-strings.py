bf1c2c5352d33788ea4e4166c43856e36a599dd1
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-01-16 23:14:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add Binary Strings/add-binary-strings.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+	def addBinary(self, A, B):
+		return bin(int(A,2)+int(B,2))[2:]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		a,b = input().split(" ")
+		ob = Solution()
+		answer = ob.addBinary(a,b)
+		
+		print(answer)
+
+
+# } Driver Code Ends
