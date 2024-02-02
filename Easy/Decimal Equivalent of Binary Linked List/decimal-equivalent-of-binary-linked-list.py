259671a7093387fa5a9813868b7d6c001bbb7745
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-03 01:19:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.py
@@ -0,0 +1,59 @@
+# your task is to complete this function
+# Function should return an integer value
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+class Solution:
+    def decimalValue(self, head):
+        m=10**9+7
+        s = 0
+        while head:
+            s = s*10 + head.data
+            head = head.next
+        s = str(s)
+        return int(s,2)%m
+
+
+#{ 
+ # Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            temp = self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = new_node
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        ob=Solution()
+        print(ob.decimalValue(list1.head))
+# Contributed By: Harshit Sidhwa
+# } Driver Code Ends
