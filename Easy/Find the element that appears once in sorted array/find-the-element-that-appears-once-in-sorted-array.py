5557a1d5339ce2064f8a7381fdb401d23277312e
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-02 23:48:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the element that appears once in sorted array/find-the-element-that-appears-once-in-sorted-array.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def findOnce(self,arr : list, n : int):
+        x = 0
+        for i in arr:
+            x ^= i
+        return x
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    
+    for _ in range(t):
+        n = int(input())
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.findOnce(arr, n))
+# } Driver Code Ends
