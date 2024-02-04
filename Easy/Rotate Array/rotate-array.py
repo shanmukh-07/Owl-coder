da71832d556cc331d68cbb0ddf0c631bece1eb0b
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-05 02:02:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotate Array/rotate-array.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        a = D%N
+        t = A[a:] + A[:a]
+        for i in range(len(t)):
+            A[i] = t[i]
+        return A
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+def main():
+    T=int(input())
+    
+    while(T>0):
+        nd=[int(x) for x in input().strip().split()]
+        N=nd[0]
+        D=nd[1]
+        A=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.rotateArr(A,D,N)
+        
+        for i in A:
+            print(i,end=" ")
+            
+        print()
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
