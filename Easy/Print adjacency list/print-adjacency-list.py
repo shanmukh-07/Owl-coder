c9fd30e95aac87277c9cb800f8bc7716af4a36be
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-13 17:30:59 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print adjacency list/print-adjacency-list.py
@@ -0,0 +1,65 @@
+
+from typing import List
+
+
+class Solution:
+    def printGraph(self, V : int, edges : List[List[int]]) -> List[List[int]]:
+        l = len(edges)
+        d = {i:[] for i in range(V)}
+        for i in range(l):
+            d[edges[i][0]].append(edges[i][1])
+            d[edges[i][1]].append(edges[i][0])
+        ll = []
+        for i in d.values():
+            ll.append(sorted(i))
+        return ll
+        
+
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+
+class IntMatrix:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n,m):
+        matrix=[]
+        #matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+    def Print(self,arr):
+        for i in arr:
+            for j in i:
+                print(j,end=" ")
+            print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        V,E=IntArray().Input()
+        
+        
+        edges=IntMatrix().Input(E, 2)
+        
+        obj = Solution()
+        res = obj.printGraph(V, edges)
+        
+        for row in res:
+            print(*sorted(row))
+# } Driver Code Ends
