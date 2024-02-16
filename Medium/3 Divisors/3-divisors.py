12882777a2f09bbce346f71ac1ea0e3a554adb04
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-02-16 20:57:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/3 Divisors/3-divisors.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+
+class Solution:
+	def threeDivisors(self, query, q):
+# 		l = []
+		def fun(n):
+		    if n<2:return False
+		    for i in range(2,int(n**0.5)+1):
+		        if n%i == 0:
+		            return False
+		    return True
+        
+# 		for i in range(2,10**6+1):
+# 		    if fun(i):
+# 		        l.append(i)
+        l = []
+        for i in query:
+            c = 0
+            for j in range(2,int(i**0.5)+1):
+                if fun(j) and j**2 <= i:
+                    c += 1
+            l.append(c)
+        return l
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		q = int(input())
+		query = []
+		for _ in range(q):
+			query.append(int(input()))
+		
+		ob = Solution()
+		ans = ob.threeDivisors(query,q)
+		for a in ans:
+			print(a)
+
+# } Driver Code Ends
