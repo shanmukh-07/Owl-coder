b4e9a380823f0902912045fba2038710030b7c12
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2024-05-25 11:54:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/You and your books/you-and-your-books.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    #Your task is to complete this function
+    #Function should return an integer
+    #a - list/array containing height of stack's respectively
+    def max_Books(self, n, k, arr):
+        ans = 0
+        t = 0
+        for i in range(n):
+            if arr[i]<=k:
+                t += arr[i]
+                ans = max(ans,t)
+            else:
+                t = 0
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        temp = list(map(int, input().strip().split()))
+        n = temp[0]
+        k = temp[1]
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.max_Books(n, k, arr))
+# Contributed by:Harshit Sidhwa
+
+# } Driver Code Ends
